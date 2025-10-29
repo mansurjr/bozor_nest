7371a7349965f0bcd636e64dfb6bac1a504ca95b
valievswe <barkamol.valiy@gmail.com>
2025-10-29 21:33:52 +0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
 
   app.enableCors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
