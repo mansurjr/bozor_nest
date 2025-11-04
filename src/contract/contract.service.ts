10d866190923a13bf08426adad540baaacb733ae
valievswe <barkamol.valiy@gmail.com>
2025-11-04 15:03:20 +0500

    order by created by

--- src/contract/contract.service.ts
+++ src/contract/contract.service.ts
@@ -221,7 +221,7 @@ export class ContractService {
     const data = await this.prisma.contract.findMany({
       where,
       include: { owner: true, store: true, createdBy: true, transactions: true },
-      orderBy: {id: 'desc'},
+      orderBy: { createdAt: 'desc' },
       skip: (page - 1) * limit,
       take: limit,
     });
